fa6da9a124797c7433fc9277b4081247f38928fd
Rick <rick_molenaar96@live.nl>
2015-12-12 12:54:25 +0100

    Added pickups.

---
+++ Game Jam 2015/Assets/PickUpManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickUpManager : MonoBehaviour {
+
+    private GameObject pickUp;
+
+    public GameObject PickUp
+    {
+        get
+        {
+            return pickUp;
+        }
+
+        set
+        {
+            pickUp = value;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+        pickUp = GameObject.FindGameObjectWithTag("pickUp");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        basicmovement player = GameObject.FindObjectOfType<basicmovement>();
+        if (Vector3.Distance(player.transform.position, pickUp.transform.position) < 1)
+        {
+            Debug.Log("Picked up pickup!");
+        }
+        //pickUp.transform.
+	}
+}
